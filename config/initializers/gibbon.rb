dbc3286576973026051fc956692dfea6bfcb9465
Alexander Kehaya <afkehaya@gmail.com>
2016-01-14 17:34:39 -0800

    added key

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,4 +1,4 @@
 MAILCHIMP_API_KEY = ENV["MAILCHIMP_KEY"]
-Gibbon::Request.api_key = "4ceb5654e6b3f0b0431134259a1e14fe-us6"
+Gibbon::Request.api_key = "MAILCHIMP_API_KEY"
 Gibbon::Request.timeout = 15
 Gibbon::Request.throws_exceptions = false
